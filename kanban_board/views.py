e9ed42457a82543442cb74a5ac6adbd7a55ecd94
Radosław Stępień <rstepien@cloudferro.com>
2020-08-23 18:30:19 +0200

    revert changes to rendering method of board

--- kanban_board/views.py
+++ kanban_board/views.py
@@ -5,15 +5,12 @@ from uuid import UUID
 
 def kanban_board(request, id):
     board = KanbanBoard.objects.get(pk=id)
-    board_elements = list(KanbanBoardElement.objects.all().select_subclasses())
-    elements_grouped = {}
-    for element in board_elements:
-        elements_grouped[element.kanban_board_state.name] = element
+    board_elements = KanbanBoardElement.objects.all().select_subclasses()
     
     return render(request, 'kanban_board/board.html', 
         context={
             "kanban_board": board, 
-            "kanban_board_elements": elements_grouped,
+            "kanban_board_elements": board_elements,
         })
 
 def board_panel(request):
