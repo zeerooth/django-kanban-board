ae3774ccd23ae10f6005c3e93abe34c877af5a20
Radosław Stępień <rstepien@cloudferro.com>
2020-08-23 15:00:09 +0200

    add KanbanBoardElement admin

--- kanban_board/admin.py
+++ kanban_board/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from kanban_board.models import KanbanBoard
+from kanban_board.models import KanbanBoard, KanbanBoardState
 
 admin.site.register(KanbanBoard)
+admin.site.register(KanbanBoardState)
