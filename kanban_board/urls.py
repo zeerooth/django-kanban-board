883e9bb4432ccfab7fdc21f265cf7fdd9f905b41
Radosław Stępień <rstepien@cloudferro.com>
2020-08-30 23:03:03 +0200

    add functionality of moving elements around

--- kanban_board/urls.py
+++ kanban_board/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name='kanban_board'
+
 urlpatterns = [
     path('<uuid:id>/', views.kanban_board),
     path('move-element/', views.change_element_status),
