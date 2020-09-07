63b74cca6570c8811159d0ebe652082d7bacab91
Radosław Stępień <rstepien@cloudferro.com>
2020-09-07 23:49:04 +0200

    Improve rendering of models in admin

--- kanban_board/admin.py
+++ kanban_board/admin.py
@@ -2,16 +2,19 @@ from django.contrib import admin
 from ordered_model.admin import OrderedStackedInline, OrderedInlineModelAdminMixin
 from kanban_board.models import KanbanBoard, KanbanBoardState, Workflow, KanbanBoardElement
 
+class KanbanBoardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'workflow', 'element_count')
+
+    def element_count(self, obj):
+        return KanbanBoardElement.objects.filter(kanban_board_parent=obj).select_subclasses().count()
+
+
 class KanbanBoardStateInline(OrderedStackedInline):
     model = KanbanBoardState
     fields = ('workflow', 'name', 'move_up_down_links', )
     readonly_fields = ('workflow', 'move_up_down_links', )
     extra = 0
     ordering = ('order',)
-    list_display = ('name', 'workflow', 'element_count')
-
-    def element_count(self, obj):
-        return KanbanBoardElement.objects.filter(kanban_board_parent=obj).select_subclasses().count()
 
 
 class WorkflowAdmin(OrderedInlineModelAdminMixin, admin.ModelAdmin):
@@ -19,8 +22,8 @@ class WorkflowAdmin(OrderedInlineModelAdminMixin, admin.ModelAdmin):
     inlines = (KanbanBoardStateInline, )
 
     def workflow_sequence(self, obj):
-        return "->".join(list(obj.kanbanboardstate_set.all()))
+        return "->".join([str(x) for x in list(obj.kanbanboardstate_set.all())])
 
-admin.site.register(KanbanBoard)
+admin.site.register(KanbanBoard, KanbanBoardAdmin)
 admin.site.register(KanbanBoardState)
 admin.site.register(Workflow, WorkflowAdmin)
